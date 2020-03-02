4597d8d5fa4b7a8ff26bbbea1b3bafa35eeb180d
Alexey Drozdov <Oleksiy.Drozdov@dish.com>
2020-03-02 18:35:47 +0200

    Current work on linux

--- auto_build_3D/auto_build_3d.cpp
+++ auto_build_3D/auto_build_3d.cpp
@@ -1,14 +1,20 @@
 #include "auto_build_3d.h"
 #include "ui_auto_build_3d.h"
+#include "AutoProfileBuilding.h"
+
 
 auto_build_3D::auto_build_3D(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::auto_build_3D)
 {
     ui->setupUi(this);
+
+    AutoBuildProfile::auto_build_parametrs_Init1();
+    abpd = new AutoBuildProfileDlg(true, NULL, AutoBuildProfile::ab);		// ...right after the dialog is closed?
 }
 
 auto_build_3D::~auto_build_3D()
 {
     delete ui;
+    delete abpd;
 }
--- auto_build_3D/auto_build_3d.h
+++ auto_build_3D/auto_build_3d.h
@@ -2,6 +2,7 @@
 #define AUTO_BUILD_3D_H
 
 #include <QMainWindow>
+#include "AutoBuildProfileDlg.h"
 
 namespace Ui {
 class auto_build_3D;
@@ -14,6 +15,8 @@ class auto_build_3D : public QMainWindow
 public:
     explicit auto_build_3D(QWidget *parent = 0);
     ~auto_build_3D();
+
+    AutoBuildProfileDlg * abpd;
 
 private:
     Ui::auto_build_3D *ui;
